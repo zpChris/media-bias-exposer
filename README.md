9fc89ee4b45750ba5ac6438c27433245aec27699
Chris Elliott <christopher7elliott@gmail.com>
2018-12-02 10:32:58 -0800

    first commit -- I have built the time tracking software and used chart.js to create pie, bar, or other graphs displaying the data

--- README.md
+++ README.md
@@ -10,38 +10,25 @@ CONTENTS OF THIS FILE
 INTRODUCTION
 ------------
 
-The No Bias News Chrome extension takes the bias and "factual reporting" ratings from <a href="https://mediabiasfactcheck.com/">Media Bias Fact Check</a> and displays these in an organized manner, along with other relevant information, when visiting the specific news website.
-
-DISCLAIMER
-----------
-
-The source we used for this project (<a href="https://mediabiasfactcheck.com/">Media Bias Fact Check</a>) has their own Chrome extension <a href="https://chrome.google.com/webstore/detail/official-media-bias-fact/hdcpibgmmcnpjmmenengjgkkfohahegk?hl=en-US">here</a>. Although we determined this while creating No Bias News, our extension still holds additional relevant information that may be interesting to you, the user.
+The Timely extension tracks your time and displays it in convenient charts, graphs, and data formats. This tool was made by zpChris.
 
 REQUIREMENTS
 ------------
 
-Since this is a Chrome extension, you have to use Google Chrome for the extension to work. Additionally, not all news sites are indexed - however, if you find a site you want Open Lens to add, please contact us at <a href="mailto:research@openlens.net?subject=Add ___ to No Bias News">research@openlens.net</a>.
+Since this is a Chrome extension, you have to use Google Chrome for the extension to work. Also, you may have to be online to use this -- haven't really checked yet.
 
 FAQ
 ---
 
 **Why should I install this extension?**
 
-In a society increasingly peppered by fake news and biased sources, fact checks and bias reports are essentially required when researching new information. This extension succintly provides the necessary information to understand the news source's perspective and subsequently adapt your interpretation of the reading.
+To easily track the time you spend online.
 
-**How is this different than the Official Media Bias Fact Check Extension?**
+**Where can I see the progression of this extension?**
 
-Aside from the design, we provide more information regarding the news organization than MBFC. However, we have not indexed as many news sources as MBFC, but we are working to expand our database.
-
-**What is Open Lens?**
-
-<a href="https://openlensresearch.weebly.com/">Open Lens</a> is a student-lead organization that promotes transparency through small tech tools, interviews, and blog posts.
-
-**Who runs Open Lens?**
-
-Open Lens is run by four students from Mercer Island: Chris Elliott, Albert Lam, Brian Park, and Minje Park. To learn more about the creators, visit our <a href="https://openlensresearch.weebly.com/about-us/">about page</a>.
+Possibly on Trello or somewhere else -- still determining this.
 
 CREATORS
 --------
 
-This project was created by <a href="https://openlensresearch.weebly.com/">Open Lens</a>, a student-lead organization that promotes transparency through small tech tools, interviews, and blog posts. Please contact <a href="mailto:research@openlens.net?subject=Sharing important research">research@openlens.net</a> to share important information, or <a href="mailto:interviews@openlens.net?subject=I'm interested in being interviewed">interviews@openlens.net</a> to arrange an interview, or <a href="mailto:hello@openlens.net?subject=Hi Open Lens!">hello@openlens.net</a> to just say hi!
+This project was created by <a href="https://chrismelliott.com">zpChris</a>.
--- popup.js
+++ popup.js
@@ -1,294 +1,177 @@
-// Initialize Firebase
-var config = {
-  apiKey: 'AIzaSyDmCjNQb6z9kwwwKP9XqWO1le9gy2W-MD4',
-  databaseURL: 'https://media-bias-exposer.firebaseio.com',
-  storageBucket: 'media-bias-exposer.appspot.com'
-};
-firebase.initializeApp(config);
+var seconds; // Integer variable to keep track of seconds
+var intervalId; // The interval ID of the current setInterval() function. Cleared on new tabs
 
-/* This is a list of subdomain cases that require the 'third' element of the 
-array to be accessed to determine the correct domain for the database */
-var subdomain_cases = ["go,com","co,uk"];
+/*
 
-// When window loads, update app
+    LISTENERS TO SET OFF OTHER FUNCTIONS
+
+*/
+
+// When window loads, update app and extension html
 window.onload = function () {
-    // Set the app to standard display settings (loading symbol)
-    document.getElementById("loaded-standard-site").style.display = "None";
-    document.getElementById("loading-site").style.display = "Block";
-    document.getElementById("loaded-news-site").style.display = "None";
-
-    chrome.tabs.query({ "active": true, "lastFocusedWindow": true }, function (tabs) {
-        var base_url = (new URL(tabs[0].url)).hostname; // stores URL hostname in base_url
-
-        base_url = base_url.split("."); // Splits components based on '.' location
-        var shortened_base_url = [base_url[base_url.length - 2], base_url[base_url.length - 1]]; // gets last two array values
-        // catch exceptions such as 'co.uk' domains (dailymail.co.uk) or 'abcnews.go.com'
-        if (subdomain_cases.includes(shortened_base_url.toString())) {
-            shortened_base_url = [base_url[base_url.length - 3], base_url[base_url.length - 2], base_url[base_url.length - 1]];
-        }
-        base_url = shortened_base_url.join(","); // re-joins base_url with ',' to allow database usability
-
-        // The URL object that holds information about the organization
-        const dbRefObject = firebase.database().ref().child(base_url);
-        /* Determine if need to update logo/icon or entire popup depending on
-           whether popup is currently open */
-        
-        update_popup(dbRefObject);
-    });
+    update_tab_info();
+    update_extension_html();
 }
 
 // When tab changes, update app
 chrome.tabs.onActivated.addListener(function () {
-    update_logo();
+    // Restart timer loop with new url
+    update_tab_info();
 });
 // When URL on same tab changes, update app
-chrome.tabs.onUpdated.addListener(function () {
-    update_logo();
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    // Since the onUpdated function fires multiple times, update it only on !undefined
+    var url = changeInfo.url;
+    if (url !== undefined) {
+        // Restart timer loop with new url
+        update_tab_info();
+    }
 });
 
-function update_logo() {
-    /* Get base URL of the current tab, get database reference using URL
-       as a key */
+
+/*
+
+    TIMER UPDATE TO CHROME.STORAGE
+
+*/
+
+function update_tab_info() {
+    
     chrome.tabs.query({ "active": true, "lastFocusedWindow": true }, function (tabs) {
         var base_url = (new URL(tabs[0].url)).hostname; // stores URL hostname in base_url
 
         base_url = base_url.split("."); // Splits components based on '.' location
         var shortened_base_url = [base_url[base_url.length - 2], base_url[base_url.length - 1]]; // gets last two array values
-        // catch exceptions such as 'co.uk' domains (dailymail.co.uk) or 'abcnews.go.com'
-        if (subdomain_cases.includes(shortened_base_url.toString())) {
-            shortened_base_url = [base_url[base_url.length - 3], base_url[base_url.length - 2], base_url[base_url.length - 1]];
-        }
-        base_url = shortened_base_url.join(","); // re-joins base_url with ',' to allow database usability
-        
-        // The URL object that holds information about the organization
-        const dbRefObject = firebase.database().ref().child(base_url);
+        base_url = shortened_base_url.join("."); // re-joins base_url with '.'
 
-        // Add different images and sizes accordingly
-        dbRefObject.child("Bias Rating").once("value", function (snap) {
-            if (snap.val() == "Left") {
-                chrome.browserAction.setIcon({
-                    path: {
-                        "16": "images/left-16.png",
-                        "32": "images/left-32.png",
-                        "48": "images/left-48.png",
-                        "64": "images/left-64.png",
-                        "128b": "images/left-128.png"
-                    }
-                });
-            } else if (snap.val() == "Left-Center") {
-                chrome.browserAction.setIcon({
-                    path: {
-                        "16": "images/left-center-16.png",
-                        "32": "images/left-center-32.png",
-                        "48": "images/left-center-48.png",
-                        "64": "images/left-center-64.png",
-                        "128b": "images/left-center-128.png"
-                    }
-                });
-            } else if (snap.val() == "Center") {
-                chrome.browserAction.setIcon({
-                    path: {
-                        "16": "images/center-16.png",
-                        "32": "images/center-32.png",
-                        "48": "images/center-48.png",
-                        "64": "images/center-64.png",
-                        "128b": "images/center-128.png"
-                    }
-                });
-            } else if (snap.val() == "Right-Center") {
-                chrome.browserAction.setIcon({
-                    path: {
-                        "16": "images/right-Center-16.png",
-                        "32": "images/right-Center-32.png",
-                        "48": "images/right-Center-48.png",
-                        "64": "images/right-Center-64.png",
-                        "128b": "images/right-Center-128.png"
-                    }
-                });
-            } else if (snap.val() == "Right") {
-                chrome.browserAction.setIcon({
-                    path: {
-                        "16": "images/right-16.png",
-                        "32": "images/right-32.png",
-                        "48": "images/right-48.png",
-                        "64": "images/right-64.png",
-                        "128b": "images/right-128.png"
-                    }
-                });
-            } else {
-                // Base logo if URL not in database (not on indexed news site)
-                chrome.browserAction.setIcon({
-                    path: {
-                        "16": "images/no-bias-news-16.png",
-                        "32": "images/no-bias-news-32.png",
-                        "48": "images/no-bias-news-48.png",
-                        "64": "images/no-bias-news-64.png",
-                        "128b": "images/no-bias-news-128.png"
-                    }
-                });
+        // Set initial value for seconds of that url
+        chrome.storage.local.get({ [base_url]: 0 }, function (result) {
+            seconds = result[base_url];
+        });
+
+        clearInterval(intervalId); // Clear the previous interval before starting anew
+
+        // Set interval ID and start the counter for that site
+        intervalId = setInterval(function () {
+            seconds++;
+            chrome.storage.local.set({ [base_url]: seconds });
+        }, 1000);
+
+    });
+}
+
+/* 
+
+    HTML UPDATE 
+
+*/
+
+// Update extension html
+function update_extension_html() {
+
+    chrome.tabs.query({ "active": true, "lastFocusedWindow": true }, function (tabs) {
+        var base_url = (new URL(tabs[0].url)).hostname; // stores URL hostname in base_url
+
+        base_url = base_url.split("."); // Splits components based on '.' location
+        var shortened_base_url = [base_url[base_url.length - 2], base_url[base_url.length - 1]]; // gets last two array values
+        base_url = shortened_base_url.join("."); // re-joins base_url with '.'
+
+        pie_chart();
+
+        // Display url
+        document.getElementById("sp-weblink").innerHTML = base_url;
+        // Fetch the seconds from chrome.storage, it will constantly update
+
+        setInterval(function () {
+
+            chrome.storage.local.get({ [base_url]: 0 }, function (result) {
+                document.getElementById("sp-webtimer").innerHTML = result[base_url] + " seconds";
+            });
+
+        }, 1000);
+
+    });
+
+}
+
+/*
+
+    DATA VISUALIZATION
+
+*/
+
+// Display pie chart
+function pie_chart() {
+
+    // Print out all keys/values in storage
+    chrome.storage.local.get(null, function (items) {
+        
+        // Sort objects by value to allow correct input into pie chart
+        var site_stats = sort_object_by_value(items);
+
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: Object.keys(site_stats),
+                datasets: [{
+                    label: 'Seconds',
+                    data: Object.values(site_stats),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(128, 128, 128, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(128, 128, 128, 1)'
+                    ],
+                    borderWidth: 1
+                }]
             }
         });
+
     });
+
 }
 
-// Access database and return corresponding URL value
-function update_popup(dbRefObject) {
-    // Changes logo based on bias rating
-    dbRefObject.once("value").then(function (snap) {
-        // If snap.exists(), URL in database and should display news info
-        if (snap.exists()) {
-            /* Check if news site isn't already loaded before changing display.
-               This also prevents the sources from being added twice, but I added
-               a catch in the display_news_html method anyway */
-            if (document.getElementById("loaded-news-site").style.display != "block") {
-                document.getElementById("loaded-standard-site").style.display = "None";
-                document.getElementById("loading-site").style.display = "Block";
-                document.getElementById("loaded-news-site").style.display = "None";
-                display_news_html(dbRefObject);
-            }
+// Sort object by value; personalized for the object function by combining remaining elements
+function sort_object_by_value(items) {
+
+    var display_number = 5;
+    var remainder = 0;
+
+    var sortable = [];
+    for (var item in items) {
+        sortable.push([item, items[item]]);
+    }
+
+    sortable.sort(function (a, b) {
+        return b[1] - a[1];
+    });
+
+    var object = {};
+
+    for (i = 0; i < sortable.length; i++) {
+        var key = Object.values(sortable)[i][0];
+        var value = Object.values(sortable)[i][1];
+        if (i > display_number-1) {
+            remainder += value;
         } else {
-            // Otherwise, display the standard popup
-            document.getElementById("loaded-standard-site").style.display = "Block";
-            document.getElementById("loading-site").style.display = "None";
-            document.getElementById("loaded-news-site").style.display = "None";
+            object[key] = value;
         }
-    });
-}
+    }
+    if (remainder != 0) {
+        object["other"] = remainder;
+    }
 
-/* DISPLAY HTML FOR NEWS SITE 
-   If a section does not have the corresponding information in the 
-   database, that section's display is set to "none" from here. */
-function display_news_html(dbRefObject) {
-    // Variables to hold database information
-    var bias_rating;
-    var bias_example;
-    var factual_reporting;
-    var notable_awards;
-    var parent_company;
-    var source_links;
-    var source_titles;
+    return object;
 
-    // Show name 
-    dbRefObject.child("Name").once("value", function (snap) {
-        document.getElementById("company-name").innerHTML = snap.val();
-        /* Once the first asynchronous function loads, and database is accessed,
-        I can show the display of the loading news site */
-        document.getElementById("loaded-standard-site").style.display = "None";
-        document.getElementById("loading-site").style.display = "None";
-        document.getElementById("loaded-news-site").style.display = "Block";
-    });
-
-    // Change background color of #bias-button based on the bias rating
-    dbRefObject.child("Bias Rating").once("value", function (snap) {
-        bias_rating = snap.val();
-        document.getElementById("bias-rating").innerHTML = bias_rating + " Bias";
-        if (bias_rating == "Right" || bias_rating == "Right-Center") {
-            document.getElementById("bias-button").style.border = "3px solid #f16f65";
-            document.getElementById("bias-button").style.color = "#f16f65";
-            document.getElementById("bias-rating").style.background = "linear-gradient(135deg, #f6afaa 0%,#f7665b 100%)";
-            if (bias_rating == "Right-Center") {
-                document.getElementById("bias-rating").style.fontSize = "18px"; // Prevent overflow while maintaining font size for other values
-            }
-        } else if (bias_rating == "Center") {
-            document.getElementById("bias-button").style.border = "3px solid #fca371";
-            document.getElementById("bias-button").style.color = "#fca371";
-            document.getElementById("bias-rating").style.background = "linear-gradient(135deg, #fad961 0%,#fc8949 100%)";
-        } else if (bias_rating == "Left" || bias_rating == "Left-Center") {
-            document.getElementById("bias-button").style.border = "3px solid #369bfe";
-            document.getElementById("bias-button").style.color = "#369bfe";
-            document.getElementById("bias-rating").style.background = "linear-gradient(135deg, #99dfff 0%,#2f95f9 100%)";
-        }
-
-    });
-
-    // Set button equal to Bias Example URL, if "None" then change text and link to pre-filled email
-    dbRefObject.child("Bias Example").once("value", function (snap) {
-        bias_example = snap.val();
-        if (bias_example == "None") {
-            document.getElementById("bias-example").innerHTML = "Send an Example";
-            document.getElementById("bias-button").setAttribute("href", "mailto:research@openlens.net?subject=New Bias Example&body=Please add the URL here: ");
-        } else {
-            document.getElementById("bias-button").setAttribute("href", bias_example);
-        }
-    });
-
-    // Factual reporting - change color based on value
-    dbRefObject.child("Factual Reporting").once("value", function (snap) {
-        factual_reporting = snap.val();
-        document.getElementById("factual-reporting").innerHTML = factual_reporting;
-        if (factual_reporting == "Very Low") {
-            document.getElementById("factual-reporting").style.color = "#A31621";
-        } else if (factual_reporting == "Low") {
-            document.getElementById("factual-reporting").style.color = "#BC555D";
-        } else if (factual_reporting == "Mixed") {
-            document.getElementById("factual-reporting").style.color = "#FFA500";
-        } else if (factual_reporting == "High") {
-            document.getElementById("factual-reporting").style.color = "#9FC957";
-        } else if (factual_reporting == "Very High") {
-            document.getElementById("factual-reporting").style.color = "#7CB518";
-        }
-    });
-
-    // Notable awards
-    dbRefObject.child("Notable Awards").once("value", function (snap) {
-        notable_awards = snap.val();
-        if (notable_awards == "None") {
-            document.getElementById("notable-awards-header").style.display = "None";
-            document.getElementById("notable-awards").style.display = "None";
-        } else {
-            document.getElementById("notable-awards").innerHTML = notable_awards;
-        }
-    });
-
-    // Parent company
-    dbRefObject.child("Parent Company").once("value", function (snap) {
-        parent_company = snap.val();
-        if (parent_company == "None") {
-            document.getElementById("parent-company-header").style.display = "None";
-            document.getElementById("parent-company").style.display = "None";
-        } else {
-            document.getElementById("parent-company").innerHTML = parent_company;
-        }
-    });
-
-    /* Add sources in sequentially after separating by semicolon and adding
-       a links from JavaScript into #sources span tags from HTML */
-    dbRefObject.child("Sources").once("value", function (snap) {
-        source_links = snap.val() + "";
-        if (source_links == "None") {
-            document.getElementById("sources-header").style.display = "None";
-
-        // Check if #sources has child elements (as sources might have already been added)
-        } else if (!document.getElementById("sources").hasChildNodes()) {
-            // The source titles I'll need for the newly created elements
-            dbRefObject.child("Source Titles").once("value", function (snap) {
-                source_titles = snap.val();
-
-                // Split the sources based on semicolon placement
-                var new_source_link = source_links.split(" ; "); // Separate the different source links
-                var new_source_title = source_titles.split(" ; "); // Same, with source titles
-
-                // Cycle through sources until all gone
-                for (var i = 0; i < new_source_link.length; i++) {
-                    var source_element = document.createElement("a"); // The actual HTML "a" element
-
-                    source_element.appendChild(document.createTextNode(new_source_title[i])); //adding title/text_node to source_element
-                    source_element.href = new_source_link[i];
-                    source_element.setAttribute("target", "_blank");
-
-                    // Add source_element to HTML doc
-                    document.getElementById("sources").appendChild(source_element);
-
-                    // If not the last element, add a comma to separate links
-                    if (i != new_source_link.length - 1) {
-                        var comma_element = document.createElement("span");
-                        comma_element.appendChild(document.createTextNode(", "));
-                        comma_element.style.color = "black";
-                        document.getElementById("sources").appendChild(comma_element);
-                    }
-                }
-
-            });
-        }
-    });
 }
